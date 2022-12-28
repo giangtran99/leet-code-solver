75c82dc762fadcedee2cc67c55c6bb94d6ab0135
Giang Tran Hoang <hoanggiangg199@gmail.com>
2022-12-29 00:29:29 +0700

    update

--- solutions/merge-sorted-array.js
+++ solutions/merge-sorted-array.js
@@ -9,7 +9,9 @@
 let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 
 var merge = function(nums1, m, nums2, n) {
-    
+    let filterdZero1 = nums1.filter(item=>item)
+    let filterdZero2 = nums2.filter(item=>item)
+
 };
 
 merge(nums1,m,nums2,n)
